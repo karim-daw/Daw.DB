3c5c8b5d6771ac1cfa327af8b18e0c58a570ec99
karim-daw <karimdaw91@gmail.com>
2024-07-26 20:12:00 +0100

    updaet gitignore

--- Daw.DB/obj/Debug/net8.0/Daw.DB.AssemblyInfo.cs
+++ Daw.DB/obj/Debug/net8.0/Daw.DB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daw.DB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6085f022aadffcf3fbd214c451d6cfb072095145")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daw.DB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daw.DB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
