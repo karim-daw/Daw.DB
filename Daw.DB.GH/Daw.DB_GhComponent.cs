a8a3edde87fd63675322f0250eda1f76ae0334b6
Karim Daw <karimdaw91@gmail.com>
2024-08-05 11:41:50 +0100

    Add Inputs for DB connection

--- Daw.DB.GH/Daw.DB_GhComponent.cs
+++ Daw.DB.GH/Daw.DB_GhComponent.cs
@@ -2,12 +2,13 @@ using Daw.DB.Data;
 using Daw.DB.Data.Interfaces;
 using Grasshopper.Kernel;
 using System;
+using System.Collections.Generic;
 
 namespace Daw.DB.GH
 {
     public class DB_GhComponent : GH_Component
     {
-        private readonly IGH_ClientApi _clientApi;
+        private readonly IGhClientApi _ghClientApi;
 
         public DB_GhComponent()
           : base("DB_GhComponent", "DB",
@@ -15,48 +16,86 @@ namespace Daw.DB.GH
             "Category", "Subcategory")
         {
             // Use the ApiFactory to get a pre-configured IClientApi instance to interact with the database
-            _clientApi = ApiFactory.GetClientApi();
+            _ghClientApi = ApiFactory.GetGhClientApi();
         }
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddBooleanParameter("Create", "C", "Boolean to trigger database creation", GH_ParamAccess.item);
             pManager.AddTextParameter("Database Name", "DB", "Name of the database to create", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Create Table", "CT", "Boolean to trigger table creation", GH_ParamAccess.item);
+            pManager.AddTextParameter("Table Name", "T", "Name of the table to create", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Add Record", "AR", "Boolean to trigger record addition", GH_ParamAccess.item);
+            pManager.AddTextParameter("RecordKeys", "RK", "Record KEYS to add to the table", GH_ParamAccess.list);
+            pManager.AddTextParameter("RecordValues", "RV", "Record VALUES to add to the table", GH_ParamAccess.list);
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("Result", "R", "Result of the database creation", GH_ParamAccess.item);
+            pManager.AddTextParameter("Result", "R", "Result of the database operation", GH_ParamAccess.item);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             bool createDatabase = false;
+            bool createTable = false;
+            bool createRecord = false;
             string databaseName = null;
+            string tableName = null;
+            // create empty lists to store the record keys 
+            List<string> recordKeys = new List<string>();
+            List<string> recordValues = new List<string>();
 
             // Retrieve input data
             if (!DA.GetData(0, ref createDatabase)) return;
             if (!DA.GetData(1, ref databaseName)) return;
+            if (!DA.GetData(2, ref createTable)) return;
+            if (!DA.GetData(3, ref tableName)) return;
+            if (!DA.GetData(4, ref createRecord)) return;
+            if (!DA.GetData(5, ref recordKeys)) return;
+            if (!DA.GetData(6, ref recordValues)) return;
 
-            // Check if the user triggered the database creation
-            if (createDatabase && !string.IsNullOrEmpty(databaseName))
+            // Create the database
+            if (createDatabase)
             {
-                try
-                {
-                    // Explicitly create the database using the user-provided name
-                    _clientApi.InitializeDatabase(databaseName);
-                    DA.SetData(0, $"Database '{databaseName}' created successfully.");
-                }
-                catch (Exception ex)
-                {
-                    // Handle and report any errors that occurred during database creation
-                    DA.SetData(0, $"Error: {ex.Message}");
-                }
+                string result = _ghClientApi.InitializeDatabase(GetDatabasePath(databaseName));
+                DA.SetData(0, result);
             }
-            else
+
+            // Create the table
+            if (createTable)
             {
-                DA.SetData(0, "Database creation not triggered or invalid database name.");
+                var columns = new Dictionary<string, string>();
+                for (int i = 0; i < recordKeys.Count; i++)
+                {
+                    columns.Add(recordKeys[i], recordValues[i]);
+                }
+                string result = _ghClientApi.CreateTable(tableName, columns);
+                DA.SetData(0, result);
             }
+
+
+            // Add a record to the table
+            if (createRecord)
+            {
+                var record = new Dictionary<string, object>();
+                for (int i = 0; i < recordKeys.Count; i++)
+                {
+                    record.Add(recordKeys[i], recordValues[i]);
+                }
+                string result = _ghClientApi.AddDictionaryRecord(tableName, record);
+                DA.SetData(0, result);
+            }
+
+
+
+
+        }
+
+
+        private string GetDatabasePath(string databaseName)
+        {
+            return $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{databaseName}.db";
         }
 
         protected override System.Drawing.Bitmap Icon => null; // Add an icon if available
