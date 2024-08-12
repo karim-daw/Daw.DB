caa89a6d4efff506ee94d06efeafd1438bce1f5b
Karim Daw <karimdaw91@gmail.com>
2024-08-12 22:59:40 +0100

    Add: error checking to lower level dictionary handlers

--- Daw.DB.Data/Interfaces/IDictionaryHandler.cs
+++ Daw.DB.Data/Interfaces/IDictionaryHandler.cs
@@ -6,8 +6,27 @@ namespace Daw.DB.Data.Interfaces
     {
         void CreateTable(string tableName, Dictionary<string, string> columns, string connectionString);
         void AddRecord(string tableName, Dictionary<string, object> record, string connectionString);
+
+
+        /// <summary>
+        /// Try to retrieve all records from a table
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
         IEnumerable<dynamic> GetAllRecords(string tableName, string connectionString);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="id"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception"></exception>
         dynamic GetRecordById(string tableName, object id, string connectionString);
+
         void UpdateRecord(string tableName, object id, Dictionary<string, object> updatedValues, string connectionString);
         void DeleteRecord(string tableName, object id, string connectionString);
     }
--- Daw.DB.Data/Services/DictionaryHandler.cs
+++ Daw.DB.Data/Services/DictionaryHandler.cs
@@ -24,7 +24,14 @@ namespace Daw.DB.Data.Services
                 db.Open();
                 var columnsDefinition = string.Join(", ", columns.Select(kv => $"{kv.Key} {kv.Value}"));
                 var createTableQuery = $"CREATE TABLE IF NOT EXISTS {tableName} ({columnsDefinition})";
-                db.Execute(createTableQuery);
+                try
+                {
+                    db.Execute(createTableQuery);
+                }
+                catch (System.Exception ex)
+                {
+                    throw new System.Exception($"Error creating table {tableName}: {ex.Message}");
+                }
             }
         }
 
@@ -36,7 +43,14 @@ namespace Daw.DB.Data.Services
                 var columns = string.Join(", ", record.Keys);
                 var values = string.Join(", ", record.Keys.Select(k => $"@{k}"));
                 var insertQuery = $"INSERT INTO {tableName} ({columns}) VALUES ({values})";
-                db.Execute(insertQuery, record);
+                try
+                {
+                    db.Execute(insertQuery, record);
+                }
+                catch (System.Exception ex)
+                {
+                    throw new System.Exception($"Error adding record to table {tableName}: {ex.Message}");
+                }
             }
         }
 
@@ -46,7 +60,19 @@ namespace Daw.DB.Data.Services
             {
                 db.Open();
                 var selectQuery = $"SELECT * FROM {tableName}";
-                return db.Query(selectQuery);
+
+                IEnumerable<dynamic> records = null;
+
+                try
+                {
+                    records = db.Query(selectQuery);
+                }
+                catch (System.Exception ex)
+                {
+                    throw new System.Exception($"Error retrieving records from table {tableName}: {ex.Message}");
+                }
+
+                return records;
             }
         }
 
