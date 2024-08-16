b9386810cdbc02b198bd1b8b4b9ddbd05e84cb1d
Karim Daw <karimdaw91@gmail.com>
2024-08-16 18:56:25 +0100

    add read records component and rename nickname par

---
+++ Daw.DB.GH/GhcReadRecords.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Daw.DB.GH
+{
+    public class GhcReadRecords : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GhcReadRecords class.
+        /// </summary>
+        public GhcReadRecords()
+          : base("GhcReadRecords", "Nickname",
+              "Description",
+              "Category", "Subcategory")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("32240D21-C21D-4B03-9245-A8C6B87F3087"); }
+        }
+    }
+}
