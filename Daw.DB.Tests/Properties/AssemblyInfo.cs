9b7705836ac9b03aaa90f4fbb66042bec6f3ce31
Karim Daw <karimdaw91@gmail.com>
2024-10-05 15:49:59 +0100

    add assembly info

---
+++ Daw.DB.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Daw.DB.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gensler")]
+[assembly: AssemblyProduct("Daw.DB.Tests")]
+[assembly: AssemblyCopyright("Copyright © Gensler 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c1bf072-42b7-409c-be82-8b67b0315c86")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
